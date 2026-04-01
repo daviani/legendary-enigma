e4b5cfba9bb90c85ad37934a114c25303062dd02
Daviani Fillatre <dev@daviani.dev>
2026-04-01 10:11:37 +0200

    test(backend): add park vehicle step definitions

---
+++ backend/step-definitions/park_vehicle.steps.ts
@@ -0,0 +1,73 @@
+import { Given, When, Then } from '@cucumber/cucumber'
+import assert from 'node:assert'
+import { Location } from '../src/Domain/Models/Location.js'
+import { ParkVehicleCommand } from '../src/App/Commands/ParkVehicle/ParkVehicleCommand.js'
+import { ParkVehicleCommandHandler } from '../src/App/Commands/ParkVehicle/ParkVehicleCommandHandler.js'
+
+Given('a location', function () {
+  this.location = new Location(48.8566, 2.3522)
+})
+
+Given('my vehicle has been parked into this location', function () {
+  const command = new ParkVehicleCommand(
+    this.myFleet.id,
+    this.vehicle.plateNumber,
+    this.location.latitude,
+    this.location.longitude,
+  )
+  const handler = new ParkVehicleCommandHandler(
+    this.fleetRepository,
+    this.vehicleRepository,
+  )
+  handler.handle(command)
+})
+
+When('I try to park my vehicle at this location', function () {
+  const command = new ParkVehicleCommand(
+    this.myFleet.id,
+    this.vehicle.plateNumber,
+    this.location.latitude,
+    this.location.longitude,
+  )
+  const handler = new ParkVehicleCommandHandler(
+    this.fleetRepository,
+    this.vehicleRepository,
+  )
+  try {
+    handler.handle(command)
+  } catch (e) {
+    this.error = e
+  }
+})
+
+When('I park my vehicle at this location', function () {
+  const command = new ParkVehicleCommand(
+    this.myFleet.id,
+    this.vehicle.plateNumber,
+    this.location.latitude,
+    this.location.longitude,
+  )
+  const handler = new ParkVehicleCommandHandler(
+    this.fleetRepository,
+    this.vehicleRepository,
+  )
+  handler.handle(command)
+})
+
+Then(
+  'the known location of my vehicle should verify this location',
+  function () {
+    const vehicle = this.vehicleRepository.findByPlateNumber(
+      this.vehicle.plateNumber,
+    )
+    assert.ok(vehicle.location)
+    assert.strictEqual(vehicle.location.equals(this.location), true)
+  },
+)
+
+Then(
+  'I should be informed that my vehicle is already parked at this location',
+  function () {
+    assert.notStrictEqual(this.error, null)
+  },
+)
